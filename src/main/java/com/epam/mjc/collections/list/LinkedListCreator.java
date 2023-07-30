23dddc1e6ddbc564ee629efb1e64273f61c6d7d4
sinke <sinkevich.work@gmail.com>
2023-07-30 12:30:35 +0300

    done

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> MyArray = new LinkedList<>();
+
+        for (Integer digital : sourceList
+        ) {
+            if (digital % 2 != 0) {
+                MyArray.addFirst(digital);
+            } else {
+                MyArray.addLast(digital);
+
+            }
+        }
+        return MyArray;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,36 @@
 package com.epam.mjc.collections.list;
 
+import java.awt.*;
 import java.util.Comparator;
 import java.util.List;
+import java.util.TreeSet;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+     ListComparator MyList = new ListComparator();
+     sourceList.sort(MyList);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        Integer A = Integer.valueOf(a);
+        Integer B = Integer.valueOf(b);
+
+        if (CalcFunction(A) > CalcFunction(B)){
+            return  1;
+        }
+        if (CalcFunction(A) < CalcFunction(B)){
+            return  -1;
+        }
+        else {
+            return A > B ?  1 :  -1;
+        }
     }
+
+    public int CalcFunction (Integer a){
+        return    (int) (5 * Math.pow(a,2)+3);
+    }
+
 }
